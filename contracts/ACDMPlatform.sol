2d1506e358ff5c53db77f19bad0b3dcdd0e6d6dd
Fedor Konovalov <fedykonovalov@gmail.com>
2022-04-28 20:52:14 +0300

    Move interface IERC20Mintable to IERC20Mintable.sol Delete unused function burn()

--- contracts/ACDMPlatform.sol
+++ contracts/ACDMPlatform.sol
@@ -3,11 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-
-interface IERC20Mintable is IERC20 {
-    function mint(address _account, uint256 _amount) external returns (bool);
-    function burn(address _account, uint256 _amount) external returns (bool);
-}
+import "./IERC20Mintable.sol";
 
 contract ACDMPlatform {
     using SafeERC20 for IERC20Mintable;
---
+++ contracts/IERC20Mintable.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IERC20Mintable is IERC20 {
+    function mint(address _account, uint256 _amount) external returns (bool);
+}

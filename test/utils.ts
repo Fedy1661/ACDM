4be0daa1d16cdf183d716195715dc5b311504698
Fedor Konovalov <fedykonovalov@gmail.com>
2022-05-05 19:37:50 +0300

    Add getBlockTimestamp() function Add return type to getTransactionFee() function

--- test/utils.ts
+++ test/utils.ts
@@ -1,12 +1,18 @@
-import { network } from "hardhat";
-import { ContractTransaction } from "ethers";
+import { ethers, network } from "hardhat";
+import { BigNumber, ContractTransaction } from "ethers";
 
 export async function increaseTime(seconds: number) {
   await network.provider.send("evm_increaseTime", [seconds]);
   await network.provider.send("evm_mine");
 }
 
-export async function getTransactionFee(tx: ContractTransaction) {
+export async function getTransactionFee(tx: ContractTransaction): Promise<BigNumber> {
   const { effectiveGasPrice, cumulativeGasUsed } = await tx.wait();
   return effectiveGasPrice.mul(cumulativeGasUsed);
+}
+
+export async function getBlockTimestamp(tx: ContractTransaction): Promise<number> {
+  const { blockNumber } = await tx.wait();
+  const block = await ethers.provider.getBlock(blockNumber);
+  return block.timestamp;
 }
